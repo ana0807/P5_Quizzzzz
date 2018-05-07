258e23bfb255240614da71a3d2e9d59d933ebd5c
Ana Martinez <ana.martinez.martin@alumnos.upm.es>
2018-05-07 17:23:31 +0200

    versiondos

--- P5_Quiz/routes/index.js
+++ P5_Quiz/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var QC = require('../control/quizz');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'CREDITS' });
+});
+
+router.get('/quizzes', QC.index);
+
 
 module.exports = router;
