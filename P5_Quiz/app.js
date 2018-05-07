3f37e75b89a4a819f594f714e821405fa9221e3f
Ana Martinez <ana.martinez.martin@alumnos.upm.es>
2018-05-07 17:30:36 +0200

    versiontres

--- P5_Quiz/app.js
+++ P5_Quiz/app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var favicon = require('serve-favicon');
 
-var indexRouter = require('./routes/index');
+var indexRouter= require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -14,17 +15,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
